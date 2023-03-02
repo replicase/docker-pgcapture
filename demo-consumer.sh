ae88e439f2f1015e71223974d93f913cfc081faf
Kenny Chen <kennychen851228@gmail.com>
2023-03-02 16:29:51 +0800

    feat: introduce pulsar ui

--- demo-consumer.sh
+++ demo-consumer.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-docker-compose up -d --build --force-recreate pulsar
-sleep 10
+docker-compose up -d --build --force-recreate pulsar pulsar-ui
+sleep 15
 docker exec pulsar sh -c "bin/pulsar-admin namespaces create public/pgcapture; bin/pulsar-admin topics create persistent://public/pgcapture/postgres"
+# consumer would auto create subscription, you do not need manual create subscription
+# docker exec pulsar sh -c "bin/pulsar-admin topics create-subscription --subscription postgres_cdc persistent://public/pgcapture/postgres"
+docker exec pulsar sh -c "bin/pulsar-admin clusters update --broker-url http://pulsar:6605 --url http://pulsar:8080 standalone"
+CSRF_TOKEN=$(curl localhost:9527/pulsar-manager/csrf-token)
+curl \
+    -H "X-XSRF-TOKEN: $CSRF_TOKEN" \
+    -H "Cookie: XSRF-TOKEN=$CSRF_TOKEN;" \
+    -H 'Content-Type: application/json' \
+    -X PUT http://localhost:9527/pulsar-manager/users/superuser \
+    -d '{"name": "admin", "password": "apachepulsar", "description": "test", "email": "username@test.org"}'
 
 docker-compose up -d --build --force-recreate postgres_source postgres_sink
 sleep 10
