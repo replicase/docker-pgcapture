ca98a56e6f56fe86925cf4064b239ce25c28f384
Kenny Chen <kennychen851228@gmail.com>
2023-03-12 15:28:33 +0800

    feat: introduce wait deps

--- demo-scheduler.sh
+++ demo-scheduler.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-docker-compose up -d --build --force-recreate pulsar
-sleep 10
+docker-compose run --rm wait-demo-scheduler-deps
 docker exec pulsar sh -c "bin/pulsar-admin namespaces create public/pgcapture; bin/pulsar-admin topics create persistent://public/pgcapture/postgres"
-
-docker-compose up -d --build --force-recreate postgres_source
-docker-compose up -d --build --force-recreate postgres_sink
-sleep 10
 
 # use configure service to poke agent to enable pulsar2pg
 # since gateway use StaticAgentPulsarResolver, and Dumper function implemented by AgentSourceDumper
-docker-compose up -d --build --force-recreate pg2pulsar agent controller gateway configure
-sleep 5
+docker-compose up -d --build --force-recreate pg2pulsar configure
