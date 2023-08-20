ca7dd9b0faa35ff033d3bd0d66f9bd452e3d6f96
Kenny Chen <kennychen851228@gmail.com>
2023-08-21 00:27:48 +0800

    feat: build latest pgcapture version

--- pgcapture/dockerbuild.sh
+++ pgcapture/dockerbuild.sh
@@ -1,19 +1,13 @@
-VERSION=${1:-v0.0.40}
+VERSION=${1:-v0.0.52}
 PLATFORM=${2:-linux/amd64}
 
 git clone --depth 1 --branch "$VERSION" https://github.com/rueian/pgcapture
 
-cd pgcapture || exit
-sha=$(git rev-parse --short HEAD)
+cd pgcapture
+
+echo "building rueian/pgcapture:$VERSION in $PLATFORM platform"
+
+PLATFORM="$PLATFORM" ./dockerbuild.sh build
+
 cd ../
-
-echo "building rueian/pgcapture:$VERSION with git sha: $sha in $PLATFORM platform"
-
-docker buildx build \
-  --platform linux/amd64 \
-  --output type=docker \
-  --build-arg VERSION="$VERSION" \
-  --build-arg SHA="$sha" \
-  -t rueian/pgcapture:"$VERSION" pgcapture
-
 rm -rf pgcapture
