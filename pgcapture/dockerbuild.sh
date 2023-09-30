6d0ab3ead79d2bb14fa81a5f6f5ad457e91411c6
kenny <kennychen851228@gmail.com>
2023-09-30 12:14:42 +0800

    use replicase/pgcapture

--- pgcapture/dockerbuild.sh
+++ pgcapture/dockerbuild.sh
@@ -1,11 +1,11 @@
-VERSION=${1:-v0.0.54}
+VERSION=${1:-v0.0.56}
 PLATFORM=${2:-linux/amd64}
 
 git clone --depth 1 --branch "$VERSION" https://github.com/replicase/pgcapture
 
 cd pgcapture
 
-echo "building rueian/pgcapture:$VERSION in $PLATFORM platform"
+echo "building replicase/pgcapture:$VERSION in $PLATFORM platform"
 
 PLATFORM="$PLATFORM" ./dockerbuild.sh build
 
